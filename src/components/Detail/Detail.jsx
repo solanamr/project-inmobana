a80edb1808a17343baa13a89c950e128dfaabfe8
solanamr <romerosolana@gmail.com>
2022-10-05 20:07:31 -0300

    terminado

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -9,11 +9,13 @@ export default function Detail({hotel}){
             <h4 className={style.address}>{hotel.address}</h4>
             <h4 className={style.review}>Review: {hotel.review === null ? "Hotel doesn't have a review" : hotel.review}</h4>
             <h4 className={style.rating}>Rating: {hotel.rating === null ? "Hotel doesn't have a rating" : hotel.rating}</h4>
+            <div className={style.divSummary}>
             <h4 className={style.summary}>{hotel.summary}</h4>
-            <h4 className={style.avg}>Average price per night: €{Math.ceil(hotel.avg_price_per_night)*1000}</h4>
-            <h4 className={style.distance}>Distance from downtown: {hotel.distance_from_downtown} km</h4>
+            </div>
+            <h4 className={style.avg}>Average price per night: €{Math.ceil(hotel.avg_price_per_night)*100}</h4>
+            <h4 className={style.distance}>Distance from downtown: {Math.ceil(hotel.distance_from_downtown)} km</h4>
             <p className={style.origin}>Origin: {hotel.origin}</p>
-            <img src={hotel.hotel_photo} alt={hotel.name} width={350} height={200} className={style.img}/>
+            <img src={hotel.hotel_photo} alt={hotel.name} width={500} height={300} className={style.img}/>
         </div>
     )
 }
